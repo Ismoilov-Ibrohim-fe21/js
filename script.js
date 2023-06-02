2fbf56b4c5db2a93c15b9277843c8ca6eb918aac
ibrohim ismoilov <ozodbekismoilov0018@gmail.com>
2023-06-02 13:23:29 +0500

    create index.html  and add HTML structure

---
+++ script.js
@@ -0,0 +1,6 @@
+let username ="Ibrohim "
+let firisname ="Ismoilov "
+let age="12 "
+let shool="6-maktabda "
+let hobby ="shaxmat "
+console.log("mening ismim " + username + "mening familyam " + firisname + "mening yoshim " + age + "men " + shool + " o'qiyman " + "mening hobbiyim " + hobby);
